b49175943f022200f8e0209ba27751dea03d81bd
Anna Teng <s12345abcd5@gmail.com>
2024-07-03 15:58:39 +0800

    修正swiper設定、lightbox載入

--- all.js
+++ all.js
@@ -8,26 +8,26 @@ $(document).ready(function () {
     var mySwiper = new Swiper('.swiper-container', {
         // 可選參數
         loop: true,
-        effect: 'page',
+        effect: 'slide',//輪播切換樣式https://swiperjs.com/swiper-api#param-effect
         autoplay: true,
         delay: 5000,
 
         // 如果需要分頁設置
-        pagination: {
-            el: '.swiper-pagination',
-        },
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
 
         // 上一個/下一個 按鈕
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+        //navigation: {
+        //     nextEl: '.swiper-button-next',
+        //    prevEl: '.swiper-button-prev',
+        // },
     });
 
     lightbox.option({
         'resizeDuration': 600,
         'wrapAround': true,
-        'positionFromTop': 1000,
+        'positionFromTop': 300,
     });
 
     $('.top_bottom').click(function (event) {
